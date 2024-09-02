bfe1370f183c3accc20b50193f104f6231bf2582
Valeriy-Perminov <perminov.ahura999@gmail.com>
2024-09-03 01:10:25 +0500

    Add files via upload

---
+++ Практическое решение Переменные.py
@@ -0,0 +1,8 @@
+homework = 12
+print(homework)
+hours_spent = 1.5
+print(hours_spent)
+course_name = 'Python'
+print(course_name)
+print(hours_spent/homework)
+print('Курс:', course_name, 'всего задач', homework, 'затрачено часов', hours_spent, 'среднее время выполнения', hours_spent/homework, 'часа.')
